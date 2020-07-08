a5f4cc04d30e13bfd34117e06f09ecdf2c5f48c1
Bjunny <wldnjs0327@naver.com>
2020-07-08 19:58:16 +0900

    Create 숫자야구.cpp

---
+++ 20200708_완전탐색/Programmers/숫자야구.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> baseball) {
+    int answer = 0;
+    int strike = 0;
+    int ball = 0;
+    
+    for (int i = 123; i <= 987; i++) {
+       string temp = to_string(i);
+       if (temp[0] == temp[1] || temp[1] == temp[2] || temp[2] == temp[0] || ?????)
+    }
+    return answer;
+}
