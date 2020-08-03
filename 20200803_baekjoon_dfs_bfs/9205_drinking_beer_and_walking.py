2b272d1abd0c5c2e37f699f39583171a4c1d3ac3
hanseokhyeon <sah0322@naver.com>
2020-08-03 12:55:17 +0900

    9205

---
+++ 20200803_baekjoon_dfs_bfs/9205_drinking_beer_and_walking.py
@@ -0,0 +1,45 @@
+"""
+9205 맥주 마시면서 걸어가기
+
+출발점, 도착점, 그리고 편의점들이 주어진다.
+50미터당 한병의 맥주를 마셔야하고 상자에는 20병의 맥주가 있다.
+편의점에 들르면 20병의 맥주를 충전할 수 있다.
+맥주가 끊이지 않고 도착할 수 있을까?
+
+알고리즘: dfs or bfs
+
+1. 현재 지점에서 도착점에 바로 갈 수 있는지 체크
+    1. 가능하면 끝
+    2. 불가능하면
+        1. 현재 위치에서 갈 수 있는 편의점을 모두 체크
+        2. 그 위치로 하나씩 이동해서 전부 체크
+"""
+
+import sys
+read = sys.stdin.readline
+
+
+def dfs(path, x, y):
+    global check
+    if abs(x-ex)+abs(y-ey) <= 1000:
+        check = True
+        return
+
+    for nx, ny in convenience_stores-set(path):
+        if abs(x-nx)+abs(y-ny) <= 1000 and not check:
+            dfs(path+[(nx, ny)], nx, ny)
+
+
+for _ in range(int(read())):
+    n = int(read())
+    sx, sy = map(int, read().split())
+    convenience_stores = {tuple(map(int, read().split())) for _ in range(n)}
+    ex, ey = map(int, read().split())
+
+    check = False
+    dfs([(sx, sy)], sx, sy)
+
+    if check:
+        print("happy")
+    else:
+        print("sad")
