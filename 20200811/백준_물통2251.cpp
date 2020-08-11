569903754544fec33cd30a3a55be537e316a392c
jooyoung0525 <pjuy0525@naver.com>
2020-08-11 20:12:26 +0900

    Update 백준_물통2251.cpp

--- 20200811/백준_물통2251.cpp
+++ 20200811/백준_물통2251.cpp
@@ -2,22 +2,21 @@
 #include<queue>
 #include<set>
 
+
 using namespace std;
 
 struct info { int a, b, c; };
 
 queue<info>Q;
 set<int>result;
-bool visit[210][210][210];
+bool visit[250][250][250];
 int A, B, C;
 
 void BFS();
 
 int main() {
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
+	
 	cin >> A >> B >> C;
 	Q.push({ A,B,C });
 	visit[A][B][C] = true;
@@ -25,7 +24,7 @@ int main() {
 
 	BFS();
 
-	set<int>::iterator it;
+	std::set<int>::iterator it;
 	for (it = result.begin(); it != result.end(); it++)
 		cout << *it << " ";
 	
@@ -39,105 +38,134 @@ void BFS() {
 		int a = Q.front().a;
 		int b = Q.front().b;
 		int c = Q.front().c;
-		Q.pop();
-
-		if (visit[a][b][c]) continue;
+        Q.pop();
+        
 		if (a == 0)result.insert(c);
-		//ac에 옮길때,
-		//a->c
-		int na = a;
-		int nb = b;
-		int nc = c;
-		int initA = A;
-		int initB = B;
-		int initC = C;
-
-		nc += na;
-		na = 0;
-		if (nc > initC) {
-			initC = nc - initC;
-			na = initC;
-		}
-		Q.push({ na,nb,nc });
-
-		//c->a
-		na = a;
-		nb = b;
-		nc = c;
-		initA = A;
-		initC = C;
-
-		na += nc;
-		nc = 0;
-		if (na > initA) {
-			initA = na - initA;
-			nc = initA;
-		}
-		Q.push({ na,nb,nc });
-
-		//b->c
-		na = a;
-		nb = b;
-		nc = c;
-		initB = B;
-		initC = C;
-
-
-		nc += nb;
-		nb = 0;
-		if (nc > initC) {
-			initC = nc - initC;
-			nb = initC;
-		}
-		Q.push({ na,nb,nc });
-
-
-		//c->b
-		na = a;
-		nb = b;
-		nc = c;
-		initB = B;
-		initC = C;
-
-		nb += nc;
-		nc = 0;
-		if (nb > initB) {
-			initB = nb - initB;
-			nc = initB;
-		}
-		Q.push({ na,nb,nc });
 		
-		//a->b
-		na = a;
-		nb = b;
-		nc = c;
-		initA = A;
-		initB = B;
+        //ac에 옮길때,
+		if (!(a==0&&c==0))
+		{
+			//a->c
+			int na = a;
+			int nb = b;
+			int nc = c;
+			int initA = A;
+			int initC = C;
 
-		nb += na;
-		na = 0;
-		if (nb > initB) {
-			initB = nb - initB;
-			na = initB;
+			nc += na;
+			na = 0;
+			if (nc > initC) {
+				initC = nc - initC;
+				na = initC;
+			}
+			
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
+
+			//c->a
+			na = a;
+			nb = b;
+			nc = c;
+			initA = A;
+			initC = C;
+
+			na += nc;
+			nc = 0;
+			if (na > initA) {
+				initA = na - initA;
+				nc = initA;
+			}
+
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
 		}
+	
+		//bc에 옮길때,
+		if (!(b == 0 && c == 0))
+		{
+			//b->c
+			int na = a;
+			int nb = b;
+			int nc = c;
+			int initB = B;
+			int initC = C;
 
-		Q.push({ na,nb,nc });
 
-		//b->a
-		na = a;
-		nb = b;
-		nc = c;
-		initA = A;
-		initB = B;
+			nc += nb;
+			nb = 0;
+			if (nc > initC) {
+				initC = nc - initC;
+				nb = initC;
+			}
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
 
-		na += nb;
-		nb = 0;
-		if (na > initA) {
-			initA = na - initA;
-			nb = initA;
+			//c->b
+			na = a;
+			nb = b;
+			nc = c;
+			initB = B;
+			initC = C;
+
+			nb += nc;
+			nc = 0;
+			if (nb > initB) {
+				initB = nb - initB;
+				nc = initB;
+			}
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
 		}
+		
+		//ab에 옮길때,
+		if (!(b == 0 && a == 0))
+		{
+			//a->b
+			int na = a;
+			int nb = b;
+			int nc = c;
+			int initA = A;
+			int initB = B;
 
-		Q.push({ na,nb,nc });
+			nb += na;
+			na = 0;
+			if (nb > initB) {
+				initB = nb - initB;
+				na = initB;
+			}
+			
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
+
+			//b->a
+			na = a;
+			nb = b;
+			nc = c;
+			initA = A;
+			initB = B;
+
+			na += nb;
+			nb = 0;
+			if (na > initA) {
+				initA = na - initA;
+				nb = initA;
+			}
+
+			if (!visit[na][nb][nc]) {
+				visit[na][nb][nc] = true;
+				Q.push({ na,nb,nc });
+			}
+		}
 		
 	}
 
